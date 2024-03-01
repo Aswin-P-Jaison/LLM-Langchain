558ab9626a83489eafa630324009a0b5a8ec88f9
Aswin P Jaison <128689792+Aswin-P-Jaison@users.noreply.github.com>
2024-03-01 10:44:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+import os
+def get_openai_response(question):
+    llm=OpenAI(openai_api_key=os.getenv("OPEN_API_KEY"),model_name="gpt-3.5-turbo-instruct",temperature=0.5)
+    response=llm(question)
+    return response
+st.set_page_config(page_title='Q and A demo')
+st.header("Langchain Application")
+input=st.text_input("Input: ",key='input')
+response=get_openai_response(input)
+submit=st.button("Ask the question")
+if submit:
+    st.subheader("The response is")
+    st.write(response)
